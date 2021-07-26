15732638fea133d2a46ef651073b803196807192
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2021-07-25 23:30:21 -0300

    Add favorite_repository

---
+++ lib/data/repository/favorite_repository_impl.dart
@@ -0,0 +1,17 @@
+import '../../domain/repository/favorite_repository.dart';
+import '../datasource/favorite_datasource.dart';
+
+class FavoriteRepositoryImpl implements FavoriteRepository {
+  final FavoriteDatasource favoriteDatasource;
+
+  FavoriteRepositoryImpl({
+    required this.favoriteDatasource,
+  });
+
+  Future<List<int>> getAll() => favoriteDatasource.getAll();
+
+  Future<void> save({required int id}) => favoriteDatasource.save(id: id);
+
+  Future<bool> isFavorite({required int id}) =>
+      favoriteDatasource.isFavorite(id: id);
+}
